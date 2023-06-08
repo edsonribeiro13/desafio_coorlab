a36e949060f37554eb3c972004c40006fa53ce3e
edsonribeiro13 <edson.viniciussr@gmail.com>
2023-06-08 19:49:12 -0300

    chore: adição do framework UI vuetify

--- code/src/main.js
+++ code/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from '@/plugins/vuetify' 
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
